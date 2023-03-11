610e5d9d05de8037bf6ed60e5666fce0af8d59b9
Amr <amr8787@yahoo.com>
2023-03-12 00:14:38 +0200

    adding files

--- src/test/java/OpenChrome.java
+++ src/test/java/OpenChrome.java
@@ -7,13 +7,13 @@ import java.time.Duration;
 public class OpenChrome {
     public static void main(String[] args) {
 
-        String browserType ="firefox";
-        if (browserType.equalsIgnoreCase("chrome")){
+        String browserType = "firefox";
+        if (browserType.equalsIgnoreCase("chrome")) {
             WebDriver driver = new ChromeDriver();
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
             driver.navigate().to("https://the-internet.herokuapp.com/login");
-        } else if (browserType=="firefox") {
+        } else if (browserType == "firefox") {
             WebDriver driver = new FirefoxDriver();
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
